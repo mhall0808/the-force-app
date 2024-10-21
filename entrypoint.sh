79357868791fcc040acf53ca0481127cf425fe25
Mark Hall <mhall0717@yahoo.com>
2024-10-20 21:02:21 -0600

    fixing entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,10 +10,7 @@ OLLAMA_PID=$!
 sleep 5
 
 # Pull the model while the server is running
-ollama pull qwen2.5:0.5b
-
-# Optionally, you can preload additional models here
-# ollama pull another-model
+ollama run qwen2.5:0.5b
 
 # Wait for the Ollama server process to keep the container running
 wait $OLLAMA_PID
